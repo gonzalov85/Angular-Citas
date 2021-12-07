50f2fc6bd11c6790911822cfa40880cf798efe72
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-07 09:18:55 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListCitasComponent } from './components/list-citas/list-citas.component';
+import { CrearCitaComponent } from './components/crear-cita/crear-cita.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCitasComponent,
+    CrearCitaComponent
   ],
   imports: [
     BrowserModule,
